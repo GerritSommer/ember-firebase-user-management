b83639cbc40934c3ce6b299002527f9972d15f11
Gerrit Sommer <gerrit.sommer@metrigo.de>
2015-09-28 21:20:45 +0200

    users

---
+++ app/models/user.js
@@ -0,0 +1,17 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+
+var attr = DS.attr;
+
+export default DS.Model.extend({
+  uid:          attr('number'),
+  firstName:    attr('string'),
+  lastName:     attr('string'),
+  email:        attr('string'),
+  gravatarLink: attr('string'),
+  createdAt:    attr('date', { defaultValue: function() { new Date().getTime(); } }),
+  updatedAt:    attr('date'),
+
+  fullName: Ember.computed('firstName', 'lastName', function() { return this.get('firstName') + ' ' + this.get('lastName'); })
+
+});
