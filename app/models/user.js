a97d40c7c765267baab0601ec973827fbec7fe73
Gerrit Sommer <gerrit.sommer@metrigo.de>
2015-10-06 09:24:03 +0200

    computed props

--- app/models/user.js
+++ app/models/user.js
@@ -10,8 +10,8 @@ export default DS.Model.extend({
   email:        attr('string'),
   gravatarLink: attr('string'),
   createdAt:    attr('date', { defaultValue: function() { new Date().getTime(); } }),
-  updatedAt:    attr('date'),
+  updatedAt:    attr('date', { defaultValue: function() { new Date().getTime(); } }),
 
-  fullName: Ember.computed('firstName', 'lastName', function() { return this.get('firstName') + ' ' + this.get('lastName'); })
+  fullName: Ember.computed('firstName', 'lastName', function() { return this.get('firstName') + ' ' + this.get('lastName'); }),
 
 });
