8881bf6b27628675fc316e401ebad03a4cebdb6f
Gerrit Sommer <gerrit.sommer@metrigo.de>
2015-10-30 16:27:47 +0100

    mirage server

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import capitalizeHelper from './helpers/capitalize';
+
+Ember.Handlebars.registerBoundHelper('capitalize', capitalizeHelper);
 
 var App;
 
