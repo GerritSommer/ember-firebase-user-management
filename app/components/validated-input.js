a3419d6a0d10a2334ad0fde4645a305e5e227459
Gerrit Sommer <gerrit.sommer@metrigo.de>
2015-10-30 16:28:46 +0100

    new input + header

---
+++ app/components/validated-input.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.TextField.extend({
+  classNameBindings: [ 'invalid:errors:lalas' ],
+
+  showErrors:        true,
+
+  invalid:           Ember.computed('errors.[]', 'showErrors', function() {
+    if (!Ember.isEmpty(this.get('errors')) && this.get('showErrors')) return true;
+  })
+
+});
