ebdedca935f70e922ef3ac589f62c62189f3c2d8
Gerrit Sommer <gerrit.sommer@metrigo.de>
2015-10-30 16:20:44 +0100

    test dummies

---
+++ tests/unit/adapters/base-user-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('adapter:base-user', 'Unit | Adapter | base user', {
+  // Specify the other units that are required for this test.
+  // needs: ['serializer:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  var adapter = this.subject();
+  assert.ok(adapter);
+});
