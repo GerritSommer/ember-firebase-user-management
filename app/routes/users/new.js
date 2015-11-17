dc73932186542e2289f32837a2bd10acf432fcfe
Gerrit Sommer <gerrit.sommer@metrigo.de>
2015-11-17 15:14:43 +0100

    remove comments

--- app/routes/users/new.js
+++ app/routes/users/new.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  authentication:     Ember.inject.service(),
+
   model: function() {
     return this.store.createRecord('user');
   },
+
   actions: {
     cancelNewUser: function(user) {
       this.store.unloadRecord(user);
@@ -45,25 +48,3 @@ export default Ember.Route.extend({
   }
 
 });
-
-
-// var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
-// ref.createUser({
-//   email: "bobtony@firebase.com",
-//   password: "correcthorsebatterystaple"
-// }, function(error, userData) {
-//   if (error) {
-//     switch (error.code) {
-//       case "EMAIL_TAKEN":
-//         console.log("The new user account cannot be created because the email is already in use.");
-//         break;
-//       case "INVALID_EMAIL":
-//         console.log("The specified email is not a valid email.");
-//         break;
-//       default:
-//         console.log("Error creating user:", error);
-//     }
-//   } else {
-//     console.log("Successfully created user account with uid:", userData.uid);
-//   }
-// });
