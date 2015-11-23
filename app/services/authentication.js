3cc2ce9736a151b2011ca5ec885908e7a89dd57c
Gerrit Sommer <gerrit.sommer@metrigo.de>
2015-11-23 12:03:01 +0100

    fix authentication logic

--- app/services/authentication.js
+++ app/services/authentication.js
@@ -4,34 +4,37 @@ export default Ember.Object.extend({
   isAuthenticating: false,
   currentUser:      null,
 
+  firebaseAdapter: Ember.computed(function() {
+    return this.store.adapterFor('application').get('firebase')
+  }),
+
   isLoggedIn: Ember.computed('currentUser', function(){
-    if ( this.store.adapterFor('application').get('firebase').getAuth() ) {
+    if ( this.get('firebaseAdapter').getAuth() ) {
       return true;
     }
   }),
 
   init: function() {
-    this.store.adapterFor('application').get('firebase').onAuth((data)=> {
+    this.get('firebaseAdapter').onAuth((data)=> {
+      this.set( 'isAuthenticating', false );
       if ( !data ) {
         this.setProperties({
-          isAuthenticating: false,
           currentUser:      null
         });
+        var router = this.container.lookup('router:main');
+        router.transitionTo('/');
       } else {
-        // TODO: replace this with queryRecord when supported
-        this.store.query('user', { uid: data.uid }).then((user)=> {
+        this.store.findRecord('user', data.uid ).then((user)=> {
           this.setProperties({
-            currentUser:      user.get('firstObject'),
-            isAuthenticating: false
+            currentUser:      user
           });
         }, (error)=> {
-          delete this.store.typeMapFor( this.store.modelFor('user') ).idToRecord[data.uid];
           this.store.createRecord('user', {
-            uid:     data.uid,
+            id:     data.uid,
+            email: data.email
           }).save().then((user) => {
             this.setProperties({
-              currentUser:      user,
-              isAuthenticating: false
+              currentUser:      user
             })
           });
         });
@@ -39,36 +42,28 @@ export default Ember.Object.extend({
     });
   },
 
-  login: function( email, password ) {
+  login: function( email, password, callback ) {
     this.set( 'isAuthenticating', true );
-    if ( !email || !password ) return;
+    if ( !email || !password ) {
+      this.set( 'isAuthenticating', false );
+      return;
+    }
 
-    this.store.adapterFor('application').get('firebase').authWithPassword({
+    this.get('firebaseAdapter').authWithPassword({
       email:    email,
       password: password
     }, (error, data)=> {
+      this.set( 'isAuthenticating', false );
       if (error) {
         console.log("There was an authentication error: ", error);
       } else {
-         this.store.findQuery( 'user', { uid: data.uid } ).then((user)=> {
-          this.set( 'currentUser', user );
-          this.set( 'isAuthenticating', false );
-        }, ()=> {
-          // delete this.store.typeMapFor(this.store.modelFor('user')).idToRecord[data.uid];
-          this.store.createRecord('user', {
-            uid:   data.uid,
-            email: data.password.email
-          }).save().then((user)=> {
-            this.set( 'currentUser', user );
-            this.set( 'isAuthenticating', false );
-          });
-        });
+        callback();
       }
     });
   },
 
   logout: function(){
-    this.store.adapterFor('application').get('firebase').unauth();
+    this.get('firebaseAdapter').unauth();
   }
 
 });
