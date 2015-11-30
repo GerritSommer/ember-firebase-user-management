5c391f24dd38f7a3ff0178bf77597f09a53bf16f
Gerrit Sommer <gerrit.sommer@metrigo.de>
2015-11-30 06:32:47 +0100

    general modal action

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,11 +6,29 @@ var uid
 export default Ember.Route.extend({
 
   actions: {
+
     error: function(erorr) {
       return this.transitionTo('login');
     },
+
     goToProfile: function(user) {
       this.transitionTo('user.edit', user)
+    },
+
+    openModal: function(modalName) {
+      return this.render(modalName, {
+        into:   'application',
+        outlet: 'model'
+      });
+    },
+
+    closeModal: function() {
+      return this.disconnectOutlet({
+        outlet:     'modal',
+        parentView: 'application'
+      });
     }
+
+
   }
 });
